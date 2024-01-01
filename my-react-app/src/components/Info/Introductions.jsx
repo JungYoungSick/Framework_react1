965b9811bc0e419ecd555e75bb45348861aa34e3
JungYoungSick <junfas1216@gmail.com>
2024-01-01 23:25:28 +0900

    도형 테스트 진행

--- my-react-app/src/components/Info/Introductions.jsx
+++ my-react-app/src/components/Info/Introductions.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Introductions({ selectedStudentName, Introduction }) {
   return (
     <>
-      <h1
+      <h2
         className={`Introduction ${selectedStudentName}`}
-      >{`${selectedStudentName}의 소개`}</h1>
+      >{`${selectedStudentName}의 소개`}</h2>
       <p className={`text2 ${Introduction}`}>{`${Introduction}`}</p>
     </>
   );
