7deb884934babc5103a8cc652b64e7f90e9862f8
JungYoungSick <junfas1216@gmail.com>
2024-01-01 18:08:31 +0900

    검색창 추가 작업 브랜치 생성

--- my-react-app/src/components/StudentInfo.jsx
+++ my-react-app/src/components/StudentInfo.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Advantages from "./Info/Advantages.jsx";
 import Introduction from "./Info/Introduction.jsx";
+import Search from "./Info/Search";
 
-export default function studentInfo({ data, selectedStudent }) {
+export default function StudentInfo({ data, selectedStudent }) {
+  const [searchedStudent, setSearchedStudent] = useState("");
+
+  const handleSearch = (studentName) => {
+    setSearchedStudent(studentName);
+  };
+
   return (
     <div className="student-Info">
       {/* 학생 소개 */}
-      <Advantages selectedStudentName={selectedStudent} />
+      <Advantages selectedStudentName={selectedStudent || searchedStudent} />
       <p className="student-Text1">{data.text1}</p>
 
       {/* 학생 장점 */}
-      <Introduction selectedStudentName={selectedStudent} />
+      <Introduction selectedStudentName={selectedStudent || searchedStudent} />
       <p className="student-Text2">{data.text2}</p>
+
+      {/* 검색창 */}
+      <Search onSearch={handleSearch} />
     </div>
-    //   <div>
-    //     <from>
-    //       <imput type='text' ID=''>Write your name.</imput>
-    //       <button type="submit">Show</button>
-    //     </from>
-    //   </div>
   );
 }
