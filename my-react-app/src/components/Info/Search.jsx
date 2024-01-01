f1bd40ed5b3e7f989e370917190e1669e297f2db
JungYoungSick <junfas1216@gmail.com>
2024-01-01 18:10:38 +0900

    테스트1, 문제: 검색창과 버튼은 생겼으나 조회불가, search버튼 클릭 시 알루트 창으로 이동

--- my-react-app/src/components/Info/Search.jsx
+++ my-react-app/src/components/Info/Search.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Search({ data }) {
+const Search = ({ onSearch }) => {
+  const [searchedStudent, setSearchedStudent] = useState("");
+
+  const handleSearch = () => {
+    // 검색 로직 추가 (프롬프트 등)
+    const studentName = prompt("Enter student name:");
+    setSearchedStudent(studentName);
+    onSearch(studentName); // 검색된 학생의 이름을 부모 컴포넌트에 전달
+  };
+
   return (
     <div>
-      <from>
-        <imput type="text" ID="search">
-          Write your name.
-        </imput>
-        <button type="submit" ID="search-But">
-          Show
-        </button>
-      </from>
+      <input type="text" id="search" value={searchedStudent} />
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
-}
+};
+
+export default Search;
