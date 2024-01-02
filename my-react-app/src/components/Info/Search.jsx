bd4317889e927fef1edf055034a4294782055880
JungYoungSick <junfas1216@gmail.com>
2024-01-02 14:51:39 +0900

    catch에서 일어나는 반복되는 오류

--- my-react-app/src/components/Info/Search.jsx
+++ my-react-app/src/components/Info/Search.jsx
@@ -1,23 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Search({ data }) {
+function Search() {
   const [inputValue, setInputValue] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
+  const [error, setError] = useState(null);
 
-  // 입력값이 변경될 때 호출되는 핸들러 함수
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  // 폼 제출 시 호출되는 핸들러 함수
-  const nameSubmit = (event) => {
+  const nameSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const response = await fetch("../Data/data.json");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      // 입력한 값과 일치하는 데이터 찾기
+      const matchingData = data.find((item) => item.name === inputValue);
+
+      if (matchingData) {
+        // 일치하는 데이터가 있을 때
+        setSearchResult({
+          advantages: matchingData.Advantages,
+          introductions: matchingData.Introductions,
+        });
+        setError(null); // 초기화
+      } else {
+        // 일치하는 데이터가 없을 때
+        setSearchResult(null);
+        setError("찾는 값이 없습니다.");
+      }
+    } catch (error) {
+      setError("오류가 있습니다.");
+    }
   };
+
+  const AdvantagesComponent = React.lazy(() => import("./Advantages.jsx"));
+  const IntroductionsComponent = React.lazy(() =>
+    import("./Introductions.jsx")
+  );
+
   return (
-    <form onSubmit={nameSubmit}>
-      {/* 입력값을 상태로 설정하고 핸들러 함수를 등록 */}
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      {/* 제출 버튼 */}
-      <button type="submit">SHOW</button>
-    </form>
+    <div>
+      <form onSubmit={nameSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Search</button>
+      </form>
+
+      {error && <div>{error}</div>}
+
+      {searchResult && (
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <AdvantagesComponent
+            selectedStudentName={inputValue}
+            Advantage={searchResult.advantages}
+          />
+          <IntroductionsComponent
+            selectedStudentName={inputValue}
+            Introduction={searchResult.introductions}
+          />
+        </React.Suspense>
+      )}
+    </div>
   );
 }
+
+export default Search;
